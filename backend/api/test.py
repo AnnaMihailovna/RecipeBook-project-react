d31511995e7476e5d2aee4cc3e4ad1736d0aa80a
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-08-25 12:30:54 +0300

    Add tests

---
+++ backend/api/test.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+from users.models import User
+
+class RecipeBookAPITestCase(TestCase):
+    def setUp(self):
+        # self.guest_client = Client()
+        self.user = User.objects.create_user(email='admin')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
+
+    def test_list_exists(self):
+        """Проверка доступности списка рецептов."""
+        response = self.authorized_client.get('/api/resipes/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    # def test_recipe_creation(self):
+    #     """Проверка создания рецепта."""
+    #     data = {'title': 'Test', 'description': 'Test'}
+    #     response = self.guest_client.post('/api/tasks/', data=data)
+    #     self.assertEqual(response.status_code, HTTPStatus.CREATED)
+    #     self.assertTrue(models.Task.objects.filter(title='Test').exists())

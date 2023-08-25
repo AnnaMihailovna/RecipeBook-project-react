63de3a6674e07903f7e29b511da6c7124470d6f4
AnnaMihailovna <anna.mihailovma@gmail.com>
2023-08-25 13:59:22 +0300

    Add tests

--- backend/api/test.py
+++ backend/api/test.py
@@ -3,19 +3,19 @@ from http import HTTPStatus
 from django.test import Client, TestCase
 
 from recipes.models import Recipe
-# from users.models import User
+from users.models import User
 
 
 class RecipeBookAPITestCase(TestCase):
     def setUp(self):
-        self.guest_client = Client()
-        # self.user = User.objects.create_user(email='admin')
-        # self.authorized_client = Client()
-        # self.authorized_client.force_login(self.user)
+        # self.guest_client = Client()
+        self.user = User.objects.create_user(email='admin')
+        self.authorized_client = Client()
+        self.authorized_client.force_login(self.user)
 
     def test_list_exists(self):
         """Проверка доступности списка рецептов."""
-        response = self.guest_client.get('/api/recipes/')
+        response = self.authorized_client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_recipe_creation(self):
@@ -25,7 +25,7 @@ class RecipeBookAPITestCase(TestCase):
                 "first_name": "Вася",
                 "last_name": "Пупкин",
                 "password": "Qwerty123"}
-        response = self.guest_client.post('/api/pecipe/', data=data)
+        response = self.authorized_client.post('/api/pecipe/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(
             Recipe.objects.filter(username='vasya.pupkin').exists()
